b2cfe7903ad1784a20217e745c27b393b444f9cc
Gabriel normandio de Lima <gabriel@gabriel.normandio>
2026-06-09 09:57:02 -0300

    User and controller

---
+++ src/main/java/com/workshop/curso/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.workshop.curso.controller;
+
+import com.workshop.curso.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User("Gabriel", "gaab@gmail.com", "(99)999999999", "password");
+        return ResponseEntity.ok().body(user);
+    }
+}
