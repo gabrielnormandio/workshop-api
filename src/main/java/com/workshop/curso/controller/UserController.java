655dc75d3b96d47d7ae1c68b092c489584d25085
Gabriel normandio de Lima <gabriel@gabriel.normandio>
2026-06-10 09:27:31 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/workshop/curso/controller/UserController.java
+++ src/main/java/com/workshop/curso/controller/UserController.java
@@ -1,18 +1,24 @@
 package com.workshop.curso.controller;
 
 import com.workshop.curso.model.User;
+import com.workshop.curso.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserRepository repository;
+
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping
     public ResponseEntity<User> findAll() {
         User user = new User("Gabriel", "gaab@gmail.com", "(99)999999999", "password");
         return ResponseEntity.ok().body(user);
     }
+
 }
